191c96d95dbc6db5d80f04055e62038187f8d454
Loveschach <codymalach@gmail.com>
2019-01-26 14:11:59 -0800

    Add walk anims to duder.

--- GGJ2019/Assets/Scripts/Player.cs
+++ GGJ2019/Assets/Scripts/Player.cs
@@ -8,9 +8,11 @@ public class Player : MonoBehaviour
 	const float SLOW_DURATION = 0.25f;
 	private float m_slowStartTime;
 	private Rigidbody m_body;
+	private Animator m_animator;
 
 	void Start () {
 		m_body = GetComponent<Rigidbody>();
+		m_animator = GetComponentInChildren<Animator>();
 	}
 
 	void Throw () {
@@ -24,12 +26,12 @@ public class Player : MonoBehaviour
 		Vector3 input = new Vector3( Input.GetAxisRaw( "Horizontal" ), 0, Input.GetAxisRaw( "Vertical" ) );
 		Vector3 velocity = input * SPEED;
 		// Slow player movement if there is no input.
-		if( input.x == 0 && input.z == 0 ) {
-			if( m_body.velocity != Vector3.zero ) {
-				m_slowStartTime = m_slowStartTime == -1 ? Time.time : m_slowStartTime;
-				float slowElapsedTime = Time.time - m_slowStartTime;
-				m_body.velocity = Vector3.Lerp( m_body.velocity, Vector3.zero, slowElapsedTime / SLOW_DURATION );
-			}
+		if( input.x == 0 && input.z == 0 && m_body.velocity != Vector3.zero ) {
+			m_slowStartTime = m_slowStartTime == -1 ? Time.time : m_slowStartTime;
+			float slowElapsedTime = Time.time - m_slowStartTime;
+			m_body.velocity = Vector3.Lerp( m_body.velocity, Vector3.zero, slowElapsedTime / SLOW_DURATION );
+			Debug.Log( "Slow percent: " + slowElapsedTime / SLOW_DURATION );
+			m_animator.SetFloat( "MoveSpeed", Mathf.Max( 1 - ( slowElapsedTime / SLOW_DURATION ), 0 ) );
 		} else {
 			// TODO: Probably want some sort of lerp here.
 			Vector3 newDir = Vector3.RotateTowards( transform.forward, input, 90, 0.0f );
@@ -37,6 +39,11 @@ public class Player : MonoBehaviour
 
 			m_body.velocity = velocity;
 			m_slowStartTime = -1;
+			Debug.Log( "We movin baby" );
+			if( input != Vector3.zero ) {
+				// TODO: Walk support ???
+				m_animator.SetFloat( "MoveSpeed", 1 );
+			}
 		}
 	}
 }
