36b71235def2b197436d73ad4451e20103abbfd2
RazMardi <razmigma89@gmail.com>
2019-01-26 12:40:48 -0800

    Modified camera scene to inlude mushroom Spawner. Added y offset based on renderer bounds on mushroom spawned.

--- GGJ2019/Assets/Scripts/MushroomSpawner.cs
+++ GGJ2019/Assets/Scripts/MushroomSpawner.cs
@@ -80,7 +80,9 @@ public class MushroomSpawner : MonoBehaviour
         int index = FindFreeMushroom();
         if( index >= 0 )
         {
-            m_mushroomPool[index].transform.position = position;
+            Renderer mushroomRenderer = m_mushroomPool[index].GetComponent<Renderer>();
+            float height = mushroomRenderer.bounds.center.y + mushroomRenderer.bounds.extents.y;
+            m_mushroomPool[index].transform.position = new Vector3( position.x, position.y + height, position.z );
             m_mushroomPool[index].SetActive( true );
             m_numMushrooms += 1;
             return SPAWN_RESULT.SUCESS;
