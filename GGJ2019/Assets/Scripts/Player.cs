baa45d4f8b439b9a3743416fdebd640e0d29fee6
Loveschach <codymalach@gmail.com>
2019-01-26 17:48:58 -0800

    Remove extra no input run.

--- GGJ2019/Assets/Scripts/Player.cs
+++ GGJ2019/Assets/Scripts/Player.cs
@@ -65,11 +65,16 @@ public class Player : MonoBehaviour
 		Vector3 moveInput = new Vector3( Input.GetAxisRaw( "Horizontal" ), 0, Input.GetAxisRaw( "Vertical" ) );
 		Vector3 velocity = moveInput * SPEED;
 		// Slow player movement if there is no input.
-		if( moveInput.x == 0 && moveInput.z == 0 && m_body.velocity != Vector3.zero ) {
-			m_slowStartTime = m_slowStartTime == -1 ? Time.time : m_slowStartTime;
-			float slowElapsedTime = Time.time - m_slowStartTime;
-			m_body.velocity = Vector3.Lerp( m_body.velocity, Vector3.zero, ( slowElapsedTime / SLOW_DURATION ) );
-			m_animator.SetFloat( "MoveSpeed", Mathf.Max( 1 - ( slowElapsedTime / SLOW_DURATION ), 0 ) );
+		if( moveInput.x == 0 && moveInput.z == 0 ) {
+			if( m_body.velocity != Vector3.zero ) {
+				m_slowStartTime = m_slowStartTime == -1 ? Time.time : m_slowStartTime;
+				float slowElapsedTime = Time.time - m_slowStartTime;
+				m_body.velocity = Vector3.Lerp( m_body.velocity, Vector3.zero, ( slowElapsedTime / SLOW_DURATION ) );
+				m_animator.SetFloat( "MoveSpeed", Mathf.Max( 1 - ( slowElapsedTime / SLOW_DURATION ), 0 ) );
+			}
+			else {
+				m_animator.SetFloat( "MoveSpeed", 0 );
+			}
 		}
 		else {
 			// TODO: Probably want some sort of lerp here.
