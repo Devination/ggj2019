9c09ffa44596a593ab0ff90835597748e4d56418
RazMardi <razmigma89@gmail.com>
2019-01-27 15:47:53 -0800

    Stats Screen

---
+++ GGJ2019/Assets/Scripts/Results.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Results : MonoBehaviour
+{
+    public Text status;
+    public Text numMushrooms;
+    private float waitTimeToReset = 0.0f;
+
+    void Start()
+    {
+        numMushrooms.text = "Number of Mushrooms: " + GameManager.numMushroomsCollected;
+        
+        int mushroomMeshIndex = GameManager.mushroomHouseIndex;
+        string statusStr = "";
+        switch ( mushroomMeshIndex )
+        {
+            case -1:
+                statusStr = "You are...Inadequate!";
+                break;
+            case 0:
+                statusStr = "You are...Decent!";
+                break;
+            case 1:
+                statusStr = "You are...Amazing!";
+                break;
+        }
+        status.text = statusStr;
+    }
+
+    private void Update()
+    {
+        waitTimeToReset += Time.deltaTime;
+        if ( Input.anyKeyDown && waitTimeToReset > 1.0f )
+        {
+            GameManager.ResetStats();
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+
+}
