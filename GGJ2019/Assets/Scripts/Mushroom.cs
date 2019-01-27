bbb3c64620f9fa29eecdccdf18551e3e0c121f51
RazMardi <razmigma89@gmail.com>
2019-01-26 16:55:25 -0800

    Mushroom bobs up and down and rotates in OnGround state

--- GGJ2019/Assets/Scripts/Mushroom.cs
+++ GGJ2019/Assets/Scripts/Mushroom.cs
@@ -79,7 +79,19 @@ public class Mushroom : MonoBehaviour
                 break;
             case MushroomState.Throw:
                 break;
+            case MushroomState.OnGround:
+                OnGroundUpdate();
+                break;
         }
+    }
+
+    private void OnGroundUpdate()
+    {
+        // bob up and down and rotate 
+        float centerY = 5.0f;
+        float totalHeight = 10.0f;
+        transform.Rotate( new Vector3( Random.value * 2.0f, Random.value * 2.0f, Random.value * 2.0f) );
+        transform.position = new Vector3( transform.position.x, centerY + Mathf.PingPong( Time.time * 5.0f, totalHeight ) - totalHeight / 2f, transform.position.z );
     }
 
     private void Update()
