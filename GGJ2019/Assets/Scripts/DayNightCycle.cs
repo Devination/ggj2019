2d8028b64cb62fb4eb84383ce43940dbbfafa7b2
Loveschach <codymalach@gmail.com>
2019-01-27 10:39:46 -0800

    No dark fast

--- GGJ2019/Assets/Scripts/DayNightCycle.cs
+++ GGJ2019/Assets/Scripts/DayNightCycle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-	public float speed = 10f;
+	public float speed = 1f;
 	public static float RotationSoFar = 0f; //Increment this and stop rotating after we've rotated 180
 	public const float MAX_ROTATION = 180f;
 
