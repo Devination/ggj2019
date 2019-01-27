3ce318d16c1ceb01fde3cb7608088f709469c4be
Devin Curry - Laptop <Devination3D@gmail.com>
2019-01-27 12:16:08 -0800

    title screen version 1

---
+++ GGJ2019/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneTransition : MonoBehaviour {
+	public float fadeSpeed = 1f;
+
+	//TODO: Get post fx and also fade those?
+
+	private DayNightCycle daynight;
+	private Image image;
+	private float daynightSpeed = 1f;
+	private bool isPlaying = false;
+
+	void Start() {
+		image = gameObject.GetComponentInChildren<Image>();
+		daynight = Object.FindObjectOfType<DayNightCycle>();
+		daynightSpeed = daynight.speed;
+		daynight.speed = 0f;
+	}
+
+	void Update() {
+		if (!isPlaying) {
+			if (Input.anyKeyDown) {
+				daynight.speed = daynightSpeed;
+				isPlaying = true;
+			}
+		}
+		else {
+			Color color = image.color;
+			color.a = Mathf.Lerp( image.color.a, 0f, fadeSpeed * Time.deltaTime );
+			image.color = color;
+		}
+	}
+}
